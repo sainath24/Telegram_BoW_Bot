ed95de8a4da5199f26c8fb8c1d0ed66389d8b991
Sainath Prasanna <sainathprasanna@gmail.com>
2020-04-22 15:00:46 +0530

    Initial setup

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import telegram
+from bot_files.cred import bot_token, bot_user_name,URL
+
+global bot
+global TOKEN
+TOKEN = bot_token
+bot = telegram.Bot(token = TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/{}'.format(TOKEN), methods = ['POST'])
+def respond():
+    update = telegram.Update.de_json(request.get_json(force = True), bot)
+
+    chat_id = update.message.chat_id
+    message_id = update.message.message_id
+
+    msg = update.message.message
+
+    bot.sendMessage(chat_id = chat_id, text = msg,reply_to_message_id=message_id)
+
+    return 'ok'
+
+@app.route('/wh',methods = ['GET'])
+def set_wh():
+    s = bot.setWebhook('{URL}{HOOK}', URL = URL,HOOK =TOKEN)
+    if s:
+        return 'wh success'
+    return 'wh failed'
+
+
+if __name__ == "__main__":
+    app.run(threaded = True)
---
+++ bot_files/cred.py
@@ -0,0 +1,3 @@
+bot_token = 1202092944:AAHgigUhX0oHAc6WIdP6Zv2WKIiByb1QjvA
+bot_user_name = Qpzmal_bot
+URL = ***REMOVED***
