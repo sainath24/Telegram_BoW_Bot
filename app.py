1a36191563dba298e57a49fb340957c20c3587bd
Sainath Prasanna <sainathprasanna@gmail.com>
2020-04-22 19:24:54 +0530

    try telegram poll

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def respond():
     b2 = telegram.InlineKeyboardButton('hey')
     b3 = telegram.InlineKeyboardButton('sai')
     b4 = telegram.InlineKeyboardButton('bye')
-    opt = telegram.InlineKeyboardMarkup([[b1,b2][b3,b4]])
+    opt = telegram.InlineKeyboardMarkup([[b1,b2],[b3,b4]])
     # poll = telegram.Poll(id='1',question='ur name?',options=['test','hey','sai','bye'],type='QUIZ',correct_option_id=3)
     bot.sendPoll(chat_id=chat_id,question='ur name?',options=['test','hey','sai','bye'],type=telegram.Poll.QUIZ,correct_option_id=3,reply_markup=opt)
     # msg = update.message.text.encode('utf-8').decode()
