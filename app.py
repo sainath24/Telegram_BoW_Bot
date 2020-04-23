de0284d07d25d1d49a9c5251889375e0485c1c83
Sainath Prasanna <sainathprasanna@gmail.com>
2020-04-23 15:21:22 +0530

    add start method and check if user is registered

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
 from flask import Flask, request
 import telegram
 from bot_files.cred import bot_token, bot_user_name,URL
+import pymongo
+from pymongo import MongoClient
 
 global bot
 global TOKEN
 TOKEN = bot_token
 bot = telegram.Bot(token = TOKEN)
 
+client = pymongo.MongoClient("mongodb+srv://***REMOVED***")
+db = client.QuizBot
+
 app = Flask(__name__)
 
-id = None
+
+def registerUser(update): 
+    # TODO: check if user is regstered if not register user
+    tid = update.message.from_user.id
+    collection = db.Users
+    user = collection.find_one({'tid':str(tid)})
+    if user:
+        message = "Hello there" + user['first_name'] + "! You're already registered and good to go! Search for a topic with /search topic_name or if you want to take a quiz, just do /quiz topic_name."
+        bot.sendMessage(chat_id=update.message.chat_id, text=message)
+        return
+    
+    # TODO: register new user
+    message = 'New User'
+    bot.sendMessage(chat_id=update.message.chat_id,text=message)
+    return
+
+
 
 @app.route('/{}'.format(TOKEN), methods = ['POST'])
 def respond():
-    global id
     update = telegram.Update.de_json(request.get_json(force = True), bot)
 
-    # if update.callback_query:
-    #     bot.answerCallbackQuery(update.callback_query.id,text='Answered')
-    #     bot.sendMessage(chat_id=chat_id,text='YAYY')
+    
     poll = update.poll
     if poll:
         vote = poll.options
@@ -30,22 +48,19 @@ def respond():
         return 'ok'
 
     chat_id = update.message.chat_id
-    id = chat_id
     message_id = update.message.message_id
+
+    msg = update.message.text.encode('utf-8').decode()
+    if msg == '/start':
+        registerUser(update)
+        return 'ok'
     
     
     
 
     opt = [[telegram.InlineKeyboardButton('test',callback_data='0'),telegram.InlineKeyboardButton('hey',callback_data='1')],[telegram.InlineKeyboardButton('sai',callback_data='2'),telegram.InlineKeyboardButton('bye',callback_data='3')]]
     opt = telegram.InlineKeyboardMarkup(inline_keyboard=opt)
-    # poll = telegram.Poll(id='1',question='ur name?',options=['test','hey','sai','bye'],type='QUIZ',correct_option_id=3)
     bot.sendPoll(chat_id=chat_id,question='ur name?',options=['test','hey','sai','bye'],type=telegram.Poll.QUIZ,correct_option_id=3,reply_markup=opt)
-    # msg = update.message.text.encode('utf-8').decode()
-
-    # bot.sendPoll(chat_id=chat_id,question='ur clg?',options=['test','hey','ssn','annuniv'],type=telegram.Poll.QUIZ,correct_option_id=2)
-    
-
-    # bot.sendMessage(chat_id = chat_id, text = msg,reply_to_message_id=message_id)
 
     return 'ok'
 
