8482a7daf5fc4f2e6ecb51e12e195dac8ce44878
Sainath Prasanna <sainathprasanna@gmail.com>
2020-04-25 19:52:01 +0530

    fix quiz search

--- app.py
+++ app.py
@@ -78,6 +78,7 @@ def registerUser(update):
         "first_name":None,
         "last_name":None,
         "interests":None,
+        "search":[],
         "past_quizzes":None,
         "current_quiz":None
     }
@@ -201,32 +202,32 @@ def genQuiz(update,topic,user,Users):
     page = driver.page_source
     soup = BeautifulSoup(page,'html.parser')
 
-    # div = soup.findAll('a',{'class':'resource-tile__link'})
-    div = soup.findAll('div',{'class':'resource-tile__content'})
+    div = soup.findAll('a',{'class':'resource-tile__link'})
+    # div = soup.findAll('div',{'class':'resource-tile__content'})
     print('links: ' + str(div))
     title_divs = soup.findAll('div',{'class':'resource-tile__title'})
     rs = []
     qcount = 0
     count = 0 #get 5 courses
-    # while count<5 and qcount<len(title_divs):
-    #     if 'quiz' in title_divs[qcount].contents[0] or 'Quiz' in title_divs[qcount].contents[0] or 'QUIZ' in title_divs[qcount].contents[0]:
-    #         rurl = div[qcount].get('href')
-    #         count+=1
-    #         addition = {}
-    #         addition['title'] = title_divs[qcount].contents[0] #Title
-    #         u = 'https://www.goconqr.com/en-US' + rurl #Link to quiz on goconqr
-    #         addition['link'] = u
-    #         rs.append(addition)
-    #     qcount+=1
-    for res in div:
-        if count<5 and ('quiz' in title_divs[count].contents[0] or 'Quiz' in title_divs[count].contents[0] or 'QUIZ' in title_divs[count].contents[0]):
-            rurl = res.find('a',{'class':'resource-tile__link'}).get('href')
+    while count<5 and qcount<len(title_divs):
+        if 'quiz' in title_divs[qcount].contents[0] or 'Quiz' in title_divs[qcount].contents[0] or 'QUIZ' in title_divs[qcount].contents[0]:
+            rurl = div[qcount].get('href')
             count+=1
             addition = {}
-            addition['title'] = title_divs[count-1].contents[0] #Title
+            addition['title'] = title_divs[qcount].contents[0] #Title
             u = 'https://www.goconqr.com/en-US' + rurl #Link to quiz on goconqr
             addition['link'] = u
             rs.append(addition)
+        qcount+=1
+    # for res in div:
+    #     if count<5 and ('quiz' in title_divs[count].contents[0] or 'Quiz' in title_divs[count].contents[0] or 'QUIZ' in title_divs[count].contents[0]):
+    #         rurl = res.find('a',{'class':'resource-tile__link'}).get('href')
+    #         count+=1
+    #         addition = {}
+    #         addition['title'] = title_divs[count-1].contents[0] #Title
+    #         u = 'https://www.goconqr.com/en-US' + rurl #Link to quiz on goconqr
+    #         addition['link'] = u
+    #         rs.append(addition)
     
     message = 'Here are some quizzes you can try,\n'
     for obj in rs:
